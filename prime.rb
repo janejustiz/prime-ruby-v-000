967662c9963b5a3e0a1102527a722ed4f0fb54b3
Jane Justiz <janejustiz@gmail.com>
2018-06-17 19:59:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  test_num = (2..(num-1)).to_a
+  if num <= 3
+    puts true
+  else
+    test_num.each do |test|
+      result = []
+      if num % test == 0
+        result << "not prime"
+      end
+      result.any?{|x| x == "not prime"}? puts false : puts true
+    end
+  end
+end
+
+num = gets.chomp
+prime?(num)
